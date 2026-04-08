faf6f99dad9721bf878771c969991488aa309c6c
Gold_RsC <310106329@qq.com>
2026-04-08 17:14:53 +0800

    2026/04/08
    ai写的f.cpp通过了，需要复盘

--- template/__int128.cpp
+++ template/__int128.cpp
@@ -27,6 +27,22 @@ __int128 read() {
     }
     return x * f;
 }
+istream& operator>>(istream& is, __int128& a) {
+    __int128 x = 0, f = 1;
+    char ch;
+    cin.get(ch);
+    while (ch < '0' || ch > '9') {
+        if (ch == '-')
+            f = -1;
+        cin.get(ch);
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = x * 10 + ch - '0';
+        cin.get(ch);
+    }
+    a = x * f;
+    return is;
+}
 void print(__int128 x) {
     if (x < 0) {
         putchar('-');
@@ -37,12 +53,24 @@ void print(__int128 x) {
     }
     putchar(x % 10 + '0');
 }
+ostream& operator<<(ostream& os, __int128 x) {
+    if (x < 0) {
+        os << '-';
+        x = -x;
+    }
+    if (x > 9) {
+        os << __int128(x / 10);
+    }
+    os << char(x % 10 + '0');
+    return os;
+}
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
 
+    cin >>
 
-    return 0;
+        return 0;
 }
--- weekly_competition/4/f.cpp
+++ weekly_competition/4/f.cpp
@@ -1,3 +1,4 @@
+// ai写的，通过了，之后详细分析一下
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -12,109 +13,108 @@
 #include <cmath>
 #include <climits>
 using namespace std;
-const int N = 16;
 
-int n;
-int m;
-int HP;
 
-int hurt[N];
-bool killed[N];
-int killnum = 1;
-int weight[N][N];
-int visited[N];
 struct State {
-    int i;
-    int hp;
-    int time;
+    int S, u, h, t;
+    bool operator>(const State& other) const {
+        return t > other.t;
+    }
 };
-int ans = INT_MAX;
 
-void dfs(State x) {
-    // cerr << "dfs" << endl;
-    if (visited[x.i] == 4) {
-        // cerr << "miss" << endl;
-        return;
-    }
-    visited[x.i]++;
-    if (!killed[x.i]) {
+int N, M, HP;
 
-        for (int j = 0; j < n; ++j) {
-            // 有前往的路
-            if (weight[x.i][j] == 0) {
-                continue;
-            }
-            if (killed[j]) {
-                continue;
-            }
-            // 不是初始节点
-            if (x.i) {
-                x.hp -= hurt[j];
-            }
-        }
-        killed[x.i] = true;
-        killnum++;
-    }
-    if (x.hp <= 0) {
-        // cerr << "die" << endl;
-        return;
-    }
-    // cerr << x.i << " " << x.hp << " " << x.time << " "
-    //  << visited[x.i] << " " << killnum << endl;
-    if (x.i == n - 1 && x.hp > 0) {
-        if (killnum == n) {
-            ans = min(ans, x.time);
-        }
-        // cerr << "end" << endl;
-        return;
-    }
-    for (int j = 0; j < n; ++j) {
-        // 有前往的路
-        if (weight[x.i][j] == 0) {
-            continue;
-        }
+const int INF = 1e9;
 
+int ans = INF;
 
-        dfs({j, x.hp, x.time + weight[x.i][j]});
-    }
-    visited[x.i]--;
-    killnum--;
-    killed[x.i] = false;
-    // cerr << "dfs-end" << endl;
-}
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    cin >> n >> m >> HP;
-    for (int i = 0; i < n; ++i) {
+    cin >> N >> M >> HP;
+
+    vector<int> hurt(N);
+    for (int i = 0; i < N; i++)
         cin >> hurt[i];
-    }
-    for (int i = 0; i < m; ++i) {
+
+    vector<vector<pair<int, int>>> g(N);
+    for (int i = 0; i < M; i++) {
         int u, v, w;
         cin >> u >> v >> w;
-        weight[v][u] = weight[u][v] = w;
+        g[u].push_back({v, w});
+        g[v].push_back({u, w});
     }
 
-    if (n == 1) {
-        cout << 0;
-    }
-    else {
-        for (int j = 0; j < n; ++j) {
-            // 有前往的路
-            if (weight[0][j] == 0) {
-                continue;
+    // 预处理 killCost[S][u]
+    vector<vector<int>> killCost(1 << N, vector<int>(N, 0));
+
+    for (int S = 0; S < (1 << N); S++) {
+        for (int u = 0; u < N; u++) {
+            if (S >> u & 1)
+                continue;  // 还没杀 u 时才需要计算
+            int damage = 0;
+            for (auto [v, w] : g[u]) {
+                if (!(S >> v & 1)) {  // v 还没被杀
+                    damage += hurt[v];
+                }
             }
-            dfs({j, HP, weight[0][j]});
+            killCost[S][u] = damage;
         }
     }
 
+    // Dijkstra
+    // dist[S][u][h] 初始 INF
 
-    if (ans == INT_MAX) {
-        cout << -1;
+    vector<vector<vector<int>>> dist(
+        1 << N,
+        vector<vector<int>>(N, vector<int>(HP + 1, INF)));
+    priority_queue<State, vector<State>, greater<State>> pq;
+
+    int startS = 0;                    // 还没杀任何怪
+    int startH = HP - killCost[0][0];  // 先杀 0 号
+    if (startH > 0) {
+        dist[1 << 0][0][startH] = 0;
+        pq.push({1 << 0, 0, startH, 0});
     }
-    else {
-        cout << ans;
+
+
+    while (!pq.empty()) {
+        State cur = pq.top();
+        pq.pop();
+        int S = cur.S, u = cur.u, h = cur.h, t = cur.t;
+        if (t > dist[S][u][h])
+            continue;
+
+        if (S == (1 << N) - 1 && u == N - 1 && h > 0) {
+            ans = min(ans, t);
+            continue;
+        }
+
+        // 1. 杀当前点 u（如果还没杀）
+        if (!(S >> u & 1)) {
+            int damage = killCost[S][u];
+            int nh     = h - damage;
+            if (nh > 0) {
+                int nS = S | (1 << u);
+                if (t < dist[nS][u][nh]) {
+                    dist[nS][u][nh] = t;
+                    pq.push({nS, u, nh, t});
+                }
+            }
+        }
+
+        // 2. 移动到邻居 v
+        for (auto [v, w] : g[u]) {
+            int nt = t + w;
+            if (nt < dist[S][v][h]) {
+                dist[S][v][h] = nt;
+                pq.push({S, v, h, nt});
+            }
+        }
     }
+
+    cout << (ans == INF ? -1 : ans) << endl;
+
     return 0;
 }
