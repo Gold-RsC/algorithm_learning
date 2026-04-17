3ae3ff366b909e29d46d9cccea5d57e3ea051262
Gold_RsC <310106329@qq.com>
2026-04-17 17:57:04 +0800

    2026/04/17

---
+++ tmp/P5666 树的重心.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef map<int,int> m;
+m a,b;
+
+
+void tree(){
+	int n;
+	cin>>n;
+	for(int i=0;i<n;++i){
+		int x,y;
+		cin>>x>>y;
+		
+	}
+}
+int main(){
+	int n;
+	cin>>n;
+	for(int i=0;i<n;++i){
+		tree();
+	}
+	return 0;
+}
+
